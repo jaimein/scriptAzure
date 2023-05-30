405bae407dbe1228808a1f68066cbf1b7616ec70
jaimein <jaimeiserte@hotmail.com>
2023-05-30 16:00:45 +0200

    ajustes

--- crearSQLEnAzure.ps1
+++ crearSQLEnAzure.ps1
@@ -17,7 +17,7 @@ $adminPassword = "pwd!DP300lab01"
 $serverName = "server-$(Get-Random)"
 
 # The sample database name
-$databaseName = "mySampleDatabase"
+$databaseName = "bd-$(Get-Random)"
 
 # The ip address range that you want to allow to access your server
 $startIp = "0.0.0.0"
@@ -29,11 +29,13 @@ Set-AzContext -Subscription $subscription.Id
 # Crear grupo de recursos
 $resourcegroup = New-AzResourceGroup -Name $resourceGroupName -Location $region
 
-$server = New-AzSqlServer 
--ServerName $servername -ResourceGroupName $resourcegroup `
+$server = New-AzSqlServer `
+-ServerName $servername `
+-ResourceGroupName $resourceGroupName `
 -Location $region `
 -SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminUsername, $(ConvertTo-SecureString -String $adminPassword -AsPlainText -Force))
 
+Write-Host "Info del server" + $server
 
 # Create a server firewall rule that allows access from the specified IP range
 $serverFirewallRule = New-AzSqlServerFirewallRule -ResourceGroupName $resourceGroupName `
@@ -47,7 +49,10 @@ $database = New-AzSqlDatabase  -ResourceGroupName $resourceGroupName `
     -RequestedServiceObjectiveName "Basic" `
     -SampleName "AdventureWorksLT"
 
+    Write-Host "Info de la base de datos" + $database
+
     # Mostrar información de la máquina virtual
 Write-Host "El server sql '$servername' se ha creado correctamente."
+Write-Host "La base de datos '$databaseName' se ha creado correctamente."
 Write-Host "Nombre de usuario administrador: $adminUsername"
 Write-Host "Contraseña: $adminPassword"
